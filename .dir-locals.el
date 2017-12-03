49ee9cc16f2bfffc00b07ca0ea07331c470b54b4
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-12-03 21:10:24 +0100

    initial main

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (intero-targets "sensei:lib" "sensei:exe:sensei-exe" "sensei:test:sensei-test")))
