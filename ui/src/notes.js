9ab66dc6ef61150e69e843d6fbc927a00b637c3c
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-12-19 13:03:19 +0100

    [wip] display notes page #11
    we need to paginate the notes by days on the server side and add the
    relevant links

--- ui/src/notes.js
+++ ui/src/notes.js
@@ -11,7 +11,7 @@ function markdownNote(note) {
 
 function formatNote(note) {
   return "<div class='note'>" +
-    markdownNote(note) +
+    new showdown.Converter({ simplifiedAutoLink: true }).makeHtml('#### ' + new Date(note.noteStart).toLocaleTimeString() + '\n\n' + note.noteContent) +
     "</div>";
 }
 
