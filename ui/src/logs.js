8df7aea7e047f15ea8db519c3b543300434b01c5
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-12-19 09:01:42 +0100

    add navigation buttons to log page #13

--- ui/src/logs.js
+++ ui/src/logs.js
@@ -3,29 +3,45 @@ import { config } from "./config";
 import { colorOf } from './color.js';
 import { get } from './request';
 
+function pagination(router, links) {
+  if (!links) return "";
+
+  return <div class='pagination'>
+    {
+      <button disabled={!links.prev} class='btn-prev' onclick={() => router.navigate(`/log/${links.prev.page}`)}>&lt;&lt;</button>
+    }
+    {
+      <button disabled={!links.next} class='btn-next' onclick={() => router.navigate(`/log/${links.next.page}`)}>&gt;&gt;</button>
+    }
+  </div>;
+}
+
 /*
   Display (latest) event log entries from the server
 */
-export default function logs() {
+export default function logs(router, page) {
   clear('main');
-  get(`/log/${config.user}`, (logEntries) => {
+  get(`/log/${config.user}?page=${page}`, (logEntries, links) => {
+    const logDiv = pagination(router, links);
     const logTable =
-      <table>
+      <table class='tbl-log'>
         <thead>
           <tr><th>Timestamp</th><th>Type</th><th>Data</th></tr>
         </thead>
         <tbody>
           {
             logEntries.map(entry => {
-              return <tr>
+              const flowType = entry._flowType ?? "Trace";
+              return <tr class={flowType}>
                 <td>{entry.timestamp ?? entry._flowState._flowStart}</td>
-                <td>{entry._flowType ?? "Trace"}</td>
+                <td>{flowType}</td>
                 <td>{JSON.stringify(entry._flowState ? entry._flowState : entry)}</td>
               </tr>;
             })
           }
         </tbody>
       </table>;
+    document.getElementById('main').appendChild(logDiv);
     document.getElementById('main').appendChild(logTable);
   });
 };
