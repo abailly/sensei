1df63665d851369d3e446878c1e41d55658f3624
Bertrand Bougon <bertrandbougon@gmail.com>
2020-12-20 16:00:07 +0100

    base on this timeline https://codepen.io/clintabrown/pen/yoFcC/, CSS and JS made\n snapshot testing added and format time

---
+++ ui/src/css-timeline.test.js
@@ -0,0 +1,54 @@
+import {dom} from './dom.js';
+import {formatISOTime} from "./date.js";
+
+import {describe, expect, test} from '@jest/globals';
+import {drawTimeline} from "./css-timeline";
+
+const flowData = [
+    {
+        "flowStart": "2020-12-17T08:30:00",
+        "flowType": "Flowing",
+        "flowEnd": "2020-12-17T09:00:00"
+    }, {
+        "flowStart": "2020-12-17T09:00:00",
+        "flowType": "Daily",
+        "flowEnd": "2020-12-17T09:30:00"
+    }, {
+        "flowStart": "2020-12-17T09:30:00",
+        "flowType": "Meeting",
+        "flowEnd": "2020-12-17T10:15:00"
+    }, {
+        "flowStart": "2020-12-17T10:15:00",
+        "flowType": "Meeting",
+        "flowEnd": "2020-12-17T12:00:00"
+    }, {
+        "flowStart": "2020-12-17T12:00:00",
+        "flowType": "Other",
+        "flowEnd": "2020-12-17T13:40:00"
+    }, {
+        "flowStart": "2020-12-17T13:40:00",
+        "flowType": "Meeting",
+        "flowEnd": "2020-12-17T14:00:00"
+    }, {
+        "flowStart": "2020-12-17T14:00:00",
+        "flowType": "Meeting",
+        "flowEnd": "2020-12-17T15:00:00"
+    }, {
+        "flowStart": "2020-12-17T15:00:00",
+        "flowType": "Rework",
+        "flowEnd": "2020-12-17T17:00:00"
+    },
+    {
+        "flowStart": "2020-12-17T17:00:00",
+        "flowType": "Meeting",
+        "flowEnd": "2020-12-17T17:00:00"
+    }
+];
+
+describe('CSS Timeline', () => {
+    test('expect timiline to be drawn', () => {
+        let container = document.createElement("div");
+        drawTimeline(container, flowData);
+        expect(container).toMatchSnapshot();
+    });
+});
--- ui/src/timeline.js
+++ ui/src/timeline.js
@@ -64,8 +64,10 @@ function createTimelineContainer(day, data, notesData) {
   details.addEventListener('change', (e) => {
     if (e.target.checked) {
       drawChart(chart, day, data, f => f.flowType);
+      drawTimeline(chart2, data);
     } else {
       drawChart(chart, day, data);
+      drawTimeline(chart2, data);
     }
   });
 
@@ -99,6 +101,7 @@ function createTimelineContainer(day, data, notesData) {
 
   document.getElementById('timelines').appendChild(container);
   drawChart(chart, day, data);
+  drawTimeline(chart2, data);
 }
 
 /**
