40adb1a93b3ade572fc768e26311c870ae614091
Bertrand Bougon <bertrandbougon@gmail.com>
2020-12-22 12:27:23 +0100

    Redefine CSS to display each timeline, overflow text do hide content.\n TODO: find a way to not fix timeline height in px

--- ui/src/css-timeline.js
+++ ui/src/css-timeline.js
@@ -1,29 +1,31 @@
-import { dom } from './dom.js';
+import {dom} from './dom.js';
 import {formatISOTime} from "./date.js";
-import { colorOf } from './color.js';
+import {colorOf} from './color.js';
 
 const THIRTY_MINUTES = 30;
 const MILLISECONDS = 1000;
 const SECONDS = 60;
 const HALF_HOUR_WIDTH = 100;
 
-function timelineFlow(flow) {
+function drawTimelineFlow(flow) {
     let flowStartDate = new Date(flow.flowStart);
     let flowEndDate = new Date(flow.flowEnd);
     const flowWidth = Math.abs((((flowEndDate.getTime() - flowStartDate.getTime()) / MILLISECONDS / SECONDS) / THIRTY_MINUTES) * HALF_HOUR_WIDTH);
     return <li style={'width:'+flowWidth+'px'}>
-        <div><h2>{formatISOTime(flowStartDate)}-{formatISOTime(flowEndDate)}</h2><h3>{flow.flowType}</h3></div>
-        <div style={'background: '+colorOf(flow.flowType)+'; position: absolute;left: 0;bottom: -36px;height: 8px;border-radius: 8px;width: 100%;'}></div></li>;
+        <div><h2>{formatISOTime(flowStartDate)}</h2><h2>{formatISOTime(flowEndDate)}</h2><h3 style="text-overflow: ellipsis;white-space: nowrap;overflow: hidden">{flow.flowType}</h3></div>
+        <div style={'background: '+colorOf(flow.flowType)+'; position: absolute;left: 0;bottom: -16px;height: 8px;border-radius: 8px;width: 100%;'}></div></li>;
 
 }
-function timelineHour(start) {
+
+function drawTimelineHour(start) {
     return <li>{formatISOTime(new Date(start))}</li>;
 
 }
 
-export function drawTimeline(container, flowData) {
-    const timelineContainer = <ul class="timeline-events" />;
-    const timelineHours = <ul class="timelines-years"/>
+export function drawTimeline(container, day, flowData) {
+    const timelineContainer = <div id={'timeline-container-' + day}/>;
+    const timelineEvents = <ul class='timeline-events'/>;
+    const timelineHours = <ul class='timelines-years'/>;
 
     function drawTimelineHours() {
         const firstFlow = flowData[0];
@@ -32,14 +34,15 @@ export function drawTimeline(container, flowData) {
         const firsFlowStartDate = new Date(firstFlow.flowStart);
         for (let i = 0; i < nbHalfHours; i++) {
             const hour = new Date(firsFlowStartDate.getFullYear(), firsFlowStartDate.getMonth(), firsFlowStartDate.getDate(), firsFlowStartDate.getHours(), firsFlowStartDate.getMinutes() + i * THIRTY_MINUTES);
-            timelineHours.appendChild(timelineHour(hour));
+            timelineHours.appendChild(drawTimelineHour(hour));
         }
     }
 
     drawTimelineHours();
     flowData.forEach(flow => {
-        timelineContainer.appendChild(timelineFlow(flow));
-        container.appendChild(timelineContainer);
+        timelineEvents.appendChild(drawTimelineFlow(flow));
+        timelineContainer.appendChild(timelineEvents);
     })
-    container.appendChild(timelineHours);
+    timelineContainer.appendChild(timelineHours);
+    container.appendChild(timelineContainer);
 }
