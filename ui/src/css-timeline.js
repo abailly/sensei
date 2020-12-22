4fe978893ca9f36627c599c7b62abf844bd6190d
Bertrand Bougon <bertrandbougon@gmail.com>
2020-12-22 18:45:43 +0100

    FIX display timeline flow (add a left margin negative or positive depending if flow starts before or after user start of day)

--- ui/src/css-timeline.js
+++ ui/src/css-timeline.js
@@ -1,27 +1,9 @@
 import {dom} from './dom.js';
-import {formatISOTime} from "./date.js";
 import {colorOf} from './color.js';
-import {DateTimeFormatter, LocalTime} from "@js-joda/core";
+import {ChronoUnit, DateTimeFormatter, LocalDateTime, LocalTime} from "@js-joda/core";
 
 const THIRTY_MINUTES = 30;
-const MILLISECONDS = 1000;
-const SECONDS = 60;
 const HALF_HOUR_WIDTH = 100;
-
-function drawTimelineFlow(flow) {
-    let flowStartDate = new Date(flow.flowStart);
-    let flowEndDate = new Date(flow.flowEnd);
-    const flowWidth = Math.abs((((flowEndDate.getTime() - flowStartDate.getTime()) / MILLISECONDS / SECONDS) / THIRTY_MINUTES) * HALF_HOUR_WIDTH);
-    return <li style={'width:'+flowWidth+'px'}>
-        <div><h2>{formatISOTime(flowStartDate)}</h2><h2>{formatISOTime(flowEndDate)}</h2><h3 style="text-overflow: ellipsis;white-space: nowrap;overflow: hidden">{flow.flowType}</h3></div>
-        <div style={'background: '+colorOf(flow.flowType)+'; position: absolute;left: 0;bottom: -16px;height: 8px;border-radius: 8px;width: 100%;'}></div></li>;
-
-}
-
-function drawTimelineHour(time) {
-    return <li>{time.format(DateTimeFormatter.ofPattern('HH:mm'))}</li>;
-
-}
 
 export function drawTimeline(container, day, userStartOfDay, userEndOfDay, flowData) {
     const timelineContainer = <div id={'timeline-container-' + day}/>;
@@ -29,10 +11,34 @@ export function drawTimeline(container, day, userStartOfDay, userEndOfDay, flowD
     const timelineEventsContainer = <div />;
     const timelineEvents = <ul class='timeline-events'/>;
     const timelineHours = <ul class='timelines-years'/>;
+    const startTime = LocalTime.parse(userStartOfDay);
+    const endTime = LocalTime.parse(userEndOfDay);
+
+    function flowLeftMargin(flow) {
+        if(flowData[0] !== flow) {
+            return 0;
+        }
+        const firstFlowStartTime = LocalDateTime.parse(flowData[0].flowStart);
+        return firstFlowStartTime.toLocalTime().compareTo(startTime) !== 0 ? (startTime.until(firstFlowStartTime.toLocalTime(), ChronoUnit.MINUTES) / THIRTY_MINUTES) * HALF_HOUR_WIDTH : 0;
+    }
+
+    function drawTimelineFlow(flow) {
+        let flowStartDate = LocalDateTime.parse(flow.flowStart);
+        let flowEndDate = LocalDateTime.parse(flow.flowEnd);
+        const flowWidth = Math.abs((flowStartDate.until(flowEndDate, ChronoUnit.MINUTES)/ THIRTY_MINUTES) * HALF_HOUR_WIDTH);
+        return <li style={'width:'+flowWidth+'px; margin-left:'+flowLeftMargin(flow)+'px'}>
+            <div><h2>{flowStartDate.format(DateTimeFormatter.ofPattern("HH:mm"))}</h2><h2>{flowEndDate.format(DateTimeFormatter.ofPattern("HH:mm"))}</h2><h3 style="text-overflow: ellipsis;white-space: nowrap;overflow: hidden">{flow.flowType}</h3></div>
+            <div style={'background: '+colorOf(flow.flowType)+'; position: absolute;left: 0;bottom: -16px;height: 8px;border-radius: 8px;width: 100%;'}></div></li>;
+
+    }
+
+    function drawTimelineHour(time) {
+        return <li>{time.format(DateTimeFormatter.ofPattern('HH:mm'))}</li>;
+
+    }
 
     function drawTimelineHours() {
-        const startTime = LocalTime.parse(userStartOfDay);
-        const nbHalfHours = (LocalTime.parse(userEndOfDay).hour() - startTime.hour()) * 2
+        const nbHalfHours = (endTime.hour() - startTime.hour()) * 2
         for (let i = 0; i < nbHalfHours; i++) {
             const time =  startTime.plusMinutes(i * THIRTY_MINUTES);
             timelineHours.appendChild(drawTimelineHour(time));
