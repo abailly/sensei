3c89987222b0ef2a18cc9c60c43b41266c5517c9
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-12-28 15:43:23 +0100

    create config and data directories

--- src/Sensei/IO.hs
+++ src/Sensei/IO.hs
@@ -1,18 +1,15 @@
 module Sensei.IO where
 
 import System.Directory
-import Control.Monad(when)
 
 getConfigDirectory :: IO FilePath
 getConfigDirectory = do
   home <- getXdgDirectory XdgConfig "sensei"
-  exists <- doesDirectoryExist home
-  when (not exists) $ createDirectory home
+  createDirectoryIfMissing True home
   pure home
 
 getDataDirectory :: IO FilePath
 getDataDirectory = do
   home <- getXdgDirectory XdgData "sensei"
-  exists <- doesDirectoryExist home
-  when (not exists) $ createDirectory home
+  createDirectoryIfMissing True home
   pure home
