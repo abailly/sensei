bb5e91cefd9d6b9b627da5784740a9c95019c211
Bertrand Bougon <bertrandbougon@gmail.com>
2021-03-03 17:05:35 +0100

    Display note content through modalby clicking on each pill (closing modal is done by clicking on close).\nTODO:\n- change close button display (with a cross)\n- remove notes modal from the dome when clicking on notes checkbox\n- rework modal display with css

--- ui/src/css-notes.test.js
+++ ui/src/css-notes.test.js
@@ -17,18 +17,38 @@ const notes = [{
 
 describe('CSS Timeline', () => {
 
+    let mainContainer;
+    let container;
+
     beforeEach(() => {
         config.userProfile = {};
+        container = document.createElement("div");
+        mainContainer = <div id="main" />;
+        container.appendChild(<div/>);
+        mainContainer.appendChild(container);
+        document.body.appendChild(mainContainer);
+    });
+
+    afterEach(() => {
+        document.body.removeChild(mainContainer);
     });
 
     test('expect notes to be drawn', () => {
         config.userProfile.userStartOfDay = '08:30:00';
         config.userProfile.userEndOfDay = '17:00:00';
-        let container = document.createElement("div");
-        container.appendChild(<div/>);
 
         drawCssNotes(container, '2020-12-17', notes);
 
-        expect(container).toMatchSnapshot();
-    })
+        expect(document.body).toMatchSnapshot();
+    });
+
+    test('expect previous drawn notes to be removed', () => {
+        config.userProfile.userStartOfDay = '08:30:00';
+        config.userProfile.userEndOfDay = '17:00:00';
+
+        drawCssNotes(container, '2020-12-17', notes);
+        drawCssNotes(container, '2020-12-17', notes);
+
+        expect(document.body).toMatchSnapshot();
+    });
 });
