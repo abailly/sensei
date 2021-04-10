dafc6aab5ec23ae090dbafc09972df4f626a4010
Arnaud Bailly <arnaud.oqube@gmail.com>
2021-04-10 09:48:46 +0200

    Minor formatting

--- test/Sensei/UISpec.hs
+++ test/Sensei/UISpec.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Sensei.UISpec where
 
-import Test.Hspec
-import Test.Hspec.Wai
 import Network.Wai
 import Sensei.Server.UI
+import Test.Hspec
+import Test.Hspec.Wai
 
 embeddedApp :: IO Application
 embeddedApp = pure $ embeddedFiles $ [("index.html", "<html></html>")]
 
 spec :: Spec
 spec =
-  with embeddedApp $ describe "Embedded UI Files" $ do
-  it "GET /index.html returns file content and 200" $ do
-    get "/index.html" `shouldRespondWith` 200
-  it "GET /foo.html returns 404" $ do
-    get "/foo.html" `shouldRespondWith` 404
+  with embeddedApp $
+    describe "Embedded UI Files" $ do
+      it "GET /index.html returns file content and 200" $ do
+        get "/index.html" `shouldRespondWith` 200
+      it "GET /foo.html returns 404" $ do
+        get "/foo.html" `shouldRespondWith` 404
