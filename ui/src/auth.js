780d3b8f4c263d5bbfc0e466e65c75f4b248b43a
Arnaud Bailly <arnaud.oqube@gmail.com>
2021-06-26 13:55:17 +0200

    Improve styling of login form

--- ui/src/auth.js
+++ ui/src/auth.js
@@ -3,9 +3,9 @@ import { post } from './request';
 
 export function login(router, container) {
   const content = <div id='login'>
-    <div><label for='username'>Login</label><input name='username' id='username' type='text' /></div>
-    <div><label for='password'>Password</label><input name='password' id='password' type='password' /></div>
-    <div><button onclick={doLogin}>Login</button></div>
+    <div class='field'><label for='username'>Login</label><input name='username' id='username' type='text' /></div>
+    <div class='field'><label for='password'>Password</label><input name='password' id='password' type='password' /></div>
+    <div class='control'><button onclick={doLogin}>Login</button></div>
   </div>;
 
   function doLogin() {
