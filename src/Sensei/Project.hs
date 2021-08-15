09764a63055658a5d5d9337063ae96f51f94d6b3
Arnaud Bailly <arnaud.oqube@gmail.com>
2021-08-15 15:04:43 +0200

    Introduce project selector function

---
+++ src/Sensei/Project.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+
+-- | Handles classification of flows by projects
+module Sensei.Project where
+
+import Data.Aeson (FromJSON, FromJSONKey, ToJSON, ToJSONKey)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Data.String (IsString)
+import Data.Text (Text, pack, unpack)
+import System.FilePath (takeBaseName)
+import Text.Regex.TDFA ((=~))
+
+newtype ProjectName = ProjectName {projectName :: Text}
+  deriving newtype (Eq, Show, Read, ToJSON, FromJSON, IsString)
+
+newtype Regex = Regex {regex :: Text}
+  deriving newtype (Eq, Show, Read, Ord, ToJSON, FromJSON, ToJSONKey, FromJSONKey, IsString)
+
+selectProject :: Map.Map Regex ProjectName -> Text -> ProjectName
+selectProject projectsMap directory =
+  fromMaybe
+    (ProjectName $ pack $ takeBaseName $ unpack directory)
+    (Map.foldrWithKey (findMatch directory) Nothing projectsMap)
+  where
+    findMatch :: Text -> Regex -> ProjectName -> Maybe ProjectName -> Maybe ProjectName
+    findMatch source (Regex re) project Nothing =
+      if source =~ re
+        then Just project
+        else Nothing
+    findMatch _ _ _ result = result
---
+++ test/Sensei/ProjectSpec.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Sensei.ProjectSpec where
+
+import qualified Data.Map as Map
+import Sensei.API
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Project Selector" $ do
+  it "returns last segment of given path given no regex matches" $ do
+    selectProject mempty "foo/bar/baz" `shouldBe` "baz"
+
+  it "returns matching project name given path given matches regex" $ do
+    selectProject (Map.fromList [(".*quux", "my project")]) "foo/bar/quux"
+      `shouldBe` "my project"
+
+  it "returns first matching project name given path given matches multiple regexes" $ do
+    selectProject (Map.fromList [(".*quux", "my project"), (".*bar.*", "other project")]) "foo/bar/quux"
+      `shouldBe` "my project"
