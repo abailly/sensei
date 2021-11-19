eff42adfc02acf17ab36bf8de04660cfe9357fb8
Arnaud Bailly <arnaud.oqube@gmail.com>
2021-11-19 07:22:46 +0100

    Add graph operator for single goal

--- test/Sensei/GraphSpec.hs
+++ test/Sensei/GraphSpec.hs
@@ -4,26 +4,28 @@
 
 module Sensei.GraphSpec where
 
-import Algebra.Graph (Graph, connect, edgeList, empty, vertex)
+import Algebra.Graph (Graph, connect, edgeList, empty, overlay, vertex)
 import Control.Monad (MonadPlus (mplus))
 import Data.Function ((&))
+import Data.Text (Text)
 import Test.Hspec
 
 spec :: Spec
 spec = do
   it "can build a simple graph" $ do
     let ops =
-          goal "Foo"
-            & link (goal "Bar" <> goal "Baz")
+          mempty
+            & goal "Foo"
+            & link (goal' "Bar" <> goal' "Baz")
 
         graph = asGraph ops
 
     edgeList graph `shouldBe` [("Bar", "Foo"), ("Baz", "Foo")]
 
-asGraph :: G -> Graph String
+asGraph :: G -> Graph Text
 asGraph = unG
 
-newtype G = G {unG :: Graph String}
+newtype G = G {unG :: Graph Text}
 
 instance Semigroup G where
   G g <> G g' = G $ g `mplus` g'
@@ -34,5 +36,8 @@ instance Monoid G where
 link :: G -> G -> G
 link (G a) (G b) = G $ a `connect` b
 
-goal :: String -> G
-goal = G . vertex
+goal :: Text -> G -> G
+goal v (G g) = G $ g `overlay` vertex v
+
+goal' :: Text -> G
+goal' = G . vertex
