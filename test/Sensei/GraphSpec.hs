d035cbc4ddfcbe89fd00356e9c586d10d5058c79
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-11-21 18:29:21 +0000

    Rewrite Graph construction as a sequence of operations

--- test/Sensei/GraphSpec.hs
+++ test/Sensei/GraphSpec.hs
@@ -4,40 +4,41 @@
 
 module Sensei.GraphSpec where
 
-import Algebra.Graph (Graph, connect, edgeList, empty, overlay, vertex)
-import Control.Monad (MonadPlus (mplus))
-import Data.Function ((&))
+import Algebra.Graph (Graph, overlay, connect, edgeList, empty, vertex)
 import Data.Text (Text)
 import Test.Hspec
 
 spec :: Spec
 spec = do
   it "can build a simple graph" $ do
-    let ops =
-          mempty
-            & goal "Foo"
-            & link (goal' "Bar" <> goal' "Baz")
+    let ops = mkG [ goal "Foo"
+            , goal "Bar"
+            , goal "Baz"
+            ]
 
         graph = asGraph ops
 
-    edgeList graph `shouldBe` [("Bar", "Foo"), ("Baz", "Foo")]
+    edgeList graph `shouldBe` [("Bar", "Foo"), ("Baz", "Bar")]
 
+mkG :: [Op ] -> G
+mkG = go (G empty empty)
+  where
+    go g [] = g
+    go (G full current) (op:ops) = 
+      case op of
+        Goal v -> go (G ((vertex v `connect` current) `overlay` full) (vertex v)) ops
+        Pop -> undefined
+
+data G =
+  G { unG :: Graph Text, currentG :: Graph Text }
+                   
 asGraph :: G -> Graph Text
 asGraph = unG
 
-newtype G = G {unG :: Graph Text}
+data Op = Goal Text | Pop
 
-instance Semigroup G where
-  G g <> G g' = G $ g `mplus` g'
+goal :: Text -> Op
+goal = Goal
 
-instance Monoid G where
-  mempty = G empty
-
-link :: G -> G -> G
-link (G a) (G b) = G $ a `connect` b
-
-goal :: Text -> G -> G
-goal v (G g) = G $ g `overlay` vertex v
-
-goal' :: Text -> G
-goal' = G . vertex
+pop :: Op
+pop = Pop
