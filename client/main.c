6e2509153e0045583469eedf9c2d6ae41aebe62f
Arnaud Bailly <arnaud.oqube@gmail.com>
2023-03-11 19:49:54 +0100

    Experiment with C TLS client
    The idea is to build a client that can be easily and quickly deployed
    on various machines to be used to provide data to sensei, so a small C
    program that can be compiled without too much hassle on various
    platforms would be ideal.
    
    Here I only wrote a SSL client that can successfully connect to a
    HTTPS server which is not super helpful as HTTP protocol is complex,
    requires JSON, etc.
    
    So the idea would be to have sensei expose a TCP port (behind TLS)
    that clients could connect to (using same authentication mechanism as
    the web server), using a "simpler" binary interface that would make
    life easier for C.

---
+++ client/main.c
@@ -0,0 +1,224 @@
+/*
+ *  Copyright 2022 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License 2.0 (the "License").  You may not use
+ *  this file except in compliance with the License.  You can obtain a copy
+ *  in the file LICENSE in the source distribution or at
+ *  https://www.openssl.org/source/license.html
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+typedef unsigned char   bool;
+#define true            1
+#define false           0
+
+SSL_CTX* create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = TLS_client_method();
+
+    ctx = SSL_CTX_new(method);
+    if (ctx == NULL) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+
+void configure_client_context(SSL_CTX *ctx)
+{
+    /*
+     * Configure the client to abort the handshake if certificate verification
+     * fails
+     */
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+
+    /*
+     * use the default system certificate trust store
+     * Can be overriden by env variables CA_PATH?
+     */
+    if (!SSL_CTX_set_default_verify_paths(ctx)) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+}
+
+void usage()
+{
+    printf("Usage: sslecho s\n");
+    printf("       --or--\n");
+    printf("       sslecho c ip\n");
+    printf("       c=client, s=server, ip=dotted ip of server\n");
+    exit(1);
+}
+
+int main(int argc, char **argv)
+{
+    int result;
+
+    SSL_CTX *ssl_ctx = NULL;
+    SSL *ssl = NULL;
+
+    int server_skt = -1;
+    int client_skt = -1;
+
+    struct addrinfo hints;
+    struct addrinfo *addr_info, *addr;
+    int resolv_err; // error code
+
+    /* used by getline relying on realloc, can't be statically allocated */
+    char *txbuf = NULL;
+    size_t txcap = 0;
+
+    int txlen;
+
+    char rxbuf[128];
+    size_t rxcap = sizeof(rxbuf);
+    int rxlen;
+
+    char *server_name = NULL;
+
+    /* Splash */
+    printf("\nsslecho : Simple TLS Client (OpenSSL 3.0.1-dev) : %s : %s\n\n", __DATE__,
+    __TIME__);
+
+    if (argc != 2) {
+      usage();
+      /* NOTREACHED */
+    }
+
+    server_name = argv[1];
+
+    /* Create context used by both client and server */
+    ssl_ctx = create_context();
+
+    {
+
+        printf("We are the client\n\n");
+
+        /* Configure client context so we verify the server correctly */
+        configure_client_context(ssl_ctx);
+
+        /* Obtain address for host */
+
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+        hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+        hints.ai_flags = 0;
+        hints.ai_protocol = 0;          /* Any protocol */
+
+        resolv_err = getaddrinfo(server_name, "443", &hints, &addr_info);
+
+        if (resolv_err != 0) {
+          fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(resolv_err));
+          exit(EXIT_FAILURE);
+        }
+
+        /* try connecting to each addr_info returned by getaddrinfo */
+        for (addr = addr_info; addr != NULL; addr = addr->ai_next) {
+          client_skt = socket(addr->ai_family, addr->ai_socktype,
+                              addr->ai_protocol);
+          if (client_skt == -1)
+            continue;
+
+          if (connect(client_skt, addr->ai_addr, addr->ai_addrlen) != -1) {
+            printf("TCP connection to server successful\n");
+            break;                  /* Success */
+          }
+
+          close(client_skt);
+          client_skt = -1;
+        }
+
+        if(client_skt == -1) {
+            perror("Unable to TCP connect to server");
+            goto exit;
+        }
+
+        /* Create client SSL structure using dedicated client socket */
+        ssl = SSL_new(ssl_ctx);
+        SSL_set_fd(ssl, client_skt);
+        /* Set hostname for SNI */
+        SSL_set_tlsext_host_name(ssl, server_name);
+        /* Configure server hostname check */
+        SSL_set1_host(ssl, server_name);
+
+        /* Now do SSL connect with server */
+        if (SSL_connect(ssl) == 1) {
+
+            printf("SSL connection to server successful\n\n");
+
+            /* Loop to send input from keyboard */
+            while (true) {
+                /* Get a line of input */
+                txlen = getline(&txbuf, &txcap, stdin);
+                /* Exit loop on error */
+                if (txlen < 0 || txbuf == NULL) {
+                    break;
+                }
+                /* Exit loop if just a carriage return */
+                if (txbuf[0] == '\n') {
+                    break;
+                }
+                /* Send it to the server */
+                if ((result = SSL_write(ssl, txbuf, txlen)) <= 0) {
+                    printf("Server closed connection\n");
+                    ERR_print_errors_fp(stderr);
+                    break;
+                }
+
+                /* Wait for the echo */
+                rxlen = SSL_read(ssl, rxbuf, rxcap);
+                if (rxlen <= 0) {
+                    printf("Server closed connection\n");
+                    ERR_print_errors_fp(stderr);
+                    break;
+                } else {
+                    /* Show it */
+                    rxbuf[rxlen] = 0;
+                    printf("Received: %s", rxbuf);
+                }
+            }
+            printf("Client exiting...\n");
+        } else {
+
+            printf("SSL connection to server failed\n\n");
+
+            ERR_print_errors_fp(stderr);
+        }
+    }
+    exit:
+    /* Close up */
+    if (ssl != NULL) {
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+    }
+    SSL_CTX_free(ssl_ctx);
+
+    if (client_skt != -1)
+        close(client_skt);
+    if (server_skt != -1)
+        close(server_skt);
+
+    if (txbuf != NULL && txcap > 0)
+        free(txbuf);
+
+    printf("sslecho exiting\n");
+
+    return 0;
+}
