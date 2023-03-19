1fcf8abf5cbf8cb18c037809d92db69b993b0c77
Arnaud Bailly <arnaud.oqube@gmail.com>
2023-03-19 17:18:36 +0100

    Add some unit tests for crlf adding function

--- client/sensei.c
+++ client/sensei.c
@@ -7,9 +7,11 @@
 size_t complete_with_crlf(char **buf, size_t *capacity, size_t len) {
   char *new_buffer = NULL;
 
+  if(len > *capacity) return -1;
+
   /* resize buffer if we need to */
   if(len == *capacity) {
-    new_buffer = realloc(buf, *capacity + 1);
+    new_buffer = realloc(*buf, *capacity + 1);
     if (!new_buffer) {
       fprintf(stderr, "failed to allocate buffer for sending %s\n", strerror(errno));
       exit(EXIT_FAILURE);
--- client/test-runner.c
+++ client/test-runner.c
@@ -1,23 +1,69 @@
 // from https://eradman.com/posts/tdd-in-c.html
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include "sensei.h"
 
 int tests_run = 0;
 
 #define FAIL(msg) printf("\nfailure in %s() line %d: %s\n", __func__, __LINE__, msg)
 #define _assert(test) do { if (!(test)) { FAIL(); return 1; } } while(0)
 #define _assert_eq(a,b) do { if ((a) != (b)) { FAIL("expected " #a " but found " #b); return 1; } } while(0)
+#define _assert_neq(a,b) do { if ((a) == (b)) { FAIL("expected " #a " and " #b " to be different"); return 1; } } while(0)
 #define _verify(test) do { int r=test(); tests_run++; if(r) return r; } while(0)
 
 
-int square_01() {
-    int x=25;
-    _assert_eq(sqrt(x), 6);
-    return 0;
+int append_crlf_to_string_without_realloc_given_it_has_enough_capacity() {
+  char *str;
+  char *old_str = NULL;
+  size_t capacity = 16;
+  size_t new_len = 0;
+
+  // allocate new string
+  str = calloc(capacity, sizeof(char));
+  old_str = str;
+
+  // complete with less than capacity
+  new_len = complete_with_crlf(&str, &capacity, 15);
+
+  _assert_eq(old_str, str);
+  _assert_eq(16, new_len);
+
+  return 0;
+}
+
+int append_crlf_to_string_with_realloc_given_it_has_not_capacity() {
+  char *str = NULL;
+  size_t capacity = 16;
+  size_t new_len = 0;
+
+  // allocate new string
+  str = malloc(capacity);
+
+  // complete with equal capacity
+  new_len = complete_with_crlf(&str, &capacity, 16);
+
+  _assert_eq(17, new_len);
+
+  return 0;
+}
+
+int returns_error_given_len_is_greater_than_capacity() {
+  char *str = NULL;
+  size_t capacity = 16;
+  int new_len = 0;
+
+  // complete with equal capacity
+  new_len = complete_with_crlf(&str, &capacity, 17);
+
+  _assert_eq(-1, new_len);
+
+  return 0;
 }
 
 int all_tests() {
-    _verify(square_01);
+    _verify(append_crlf_to_string_without_realloc_given_it_has_enough_capacity);
+    _verify(append_crlf_to_string_with_realloc_given_it_has_not_capacity);
+    _verify(returns_error_given_len_is_greater_than_capacity);
     return 0;
 }
 
