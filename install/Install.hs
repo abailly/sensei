5952aeb1708fa4eea842d894bbfc1a2c7e0fb3f4
Arnaud Bailly <arnaud.oqube@gmail.com>
2023-06-03 16:33:37 +0200

    Simplify build.hs removing Docker stuff

--- install/Install.hs
+++ install/Install.hs
@@ -31,13 +31,6 @@ options =
           -- Rebuild only if content changes, not if date/time changes
         }
 
-checkEnvironment :: Action ()
-checkEnvironment = do
-    creds <- getEnv "GOOGLE_APPLICATION_CREDENTIALS"
-    case creds of
-        Nothing -> throw (userError $ "GOOGLE_APPLICATION_CREDENTIALS environment variable is not set")
-        Just _ -> pure ()
-
 install :: IO ()
 install = do
     pwd <- getCurrentDirectory
@@ -53,7 +46,6 @@ runShake pwd uid = shakeArgs options $ do
     addBuiltinContainerRule
 
     let needHaskellSources = do
-            needImage "pankzsoft/haskell-base"
             needDirectoryFiles
                 "."
                 [ "src//*.hs"
@@ -61,9 +53,8 @@ runShake pwd uid = shakeArgs options $ do
                 , "data//*.*"
                 , "app//*.hs"
                 , "package.yaml"
-                , "Dockerfile"
                 ]
-            need ["stack.yaml"]
+            need ["stack.yaml", "sensei.cabal", "cabal.project" ]
             need ["ui/dist/index.html"]
 
         needClientSources =
@@ -73,61 +64,17 @@ runShake pwd uid = shakeArgs options $ do
                 , "Makefile"
                 ]
 
-    -- Build all images locally
-    imageRule "pankzsoft/haskell-base" $ \img -> do
-        need ["haskell-base/Dockerfile", "haskell-base/haskell.key"]
-        cmd "docker" ["build", "--platform", "linux/x86_64", "--build-arg", "STACKAGE_VERSION=" <> stackage_version, "-t", toArg img, "haskell-base"]
-
-    "build" ~> void (needImage (docker_repository </> "sensei"))
-
-    "test" ~> do
-        needHaskellSources
-        cmd
-            "docker"
-            [ "run"
-            , "--rm"
-            , "-t"
-            , "-v"
-            , pwd <> ":/work"
-            , "-v"
-            , "haskell-stack:/home/user/.stack"
-            , "-w"
-            , "/work"
-            , "-e"
-            , "LOCAL_USER_ID=" <> uid
-            , "pankzsoft/haskell-base"
-            , "stack"
-            , "test"
-            , "--allow-different-user"
-            , "--work-dir=.stack-work-build"
-            ]
-
-    imageRule (docker_repository </> "sensei") $ \img -> do
-        need ["bin/sensei-exe"]
-        cmd "docker" ["build", "-t", toArg img, "-f", "Dockerfile", "."]
+    "build" ~> void (need ["bin/sensei-exe"])
 
     "client/senseic" %> \clientbin -> do
         needClientSources
-        cmd "make" [ "-C", "client" ]
+        cmd "make" ["-C", "client"]
 
     "bin/sensei-exe" %> \bin -> do
         needHaskellSources
         cmd
-            "docker"
-            [ "run"
-            , "--rm"
-            , "-t"
-            , "-v"
-            , pwd <> ":/work"
-            , "-v"
-            , "haskell-stack:/home/user/.stack"
-            , "-w"
-            , "/work"
-            , "-e"
-            , "LOCAL_USER_ID=" <> uid
-            , "pankzsoft/haskell-base"
-            , "stack"
-            , "install"
+            "stack"
+            [ "install"
             , "--allow-different-user"
             , "--work-dir=.stack-work-build"
             , "--test"
@@ -140,23 +87,9 @@ runShake pwd uid = shakeArgs options $ do
         hd <- askGit (GitHead ())
         case hd of
             Just sha1 -> do
-                void (needImage $ docker_repository </> "sensei")
-                forM_ ["sensei", "haskell-base"] $ \img -> do
-                    cmd_ "docker" ["tag", docker_repository </> img <> ":latest", docker_repository </> img <> ":" <> sha1]
-                    cmd_ "docker" ["push", docker_repository </> img <> ":" <> sha1]
+                need ["bin/sensei-exe"]
+                cmd_ "git" ["push", "origin", sha1 <> ":deploy"]
             Nothing -> putInfo "git working directory not clean, so not pushing anything"
-
-    -- Configure the cloud environment to host the services
-    -- Most of the work is delegated to terraform
-    "deploy" ~> do
-        checkEnvironment
-        cmd_ (Cwd "infra") "terraform" ["plan", "-out=.deploy.plan", "-var-file=terraform.tfvars"]
-        cmd_ (Cwd "infra") "terraform" ["apply", ".deploy.plan"]
-
-    -- Clean up all resources set for the deployment
-    "destroy" ~> do
-        checkEnvironment
-        cmd_ (Cwd "infra") "terraform" ["destroy", "-auto-approve", "-var-file=terraform.tfvars"]
 
     "ui/dist/index.html" %> \_ -> do
         needDirectoryFiles
@@ -168,20 +101,8 @@ runShake pwd uid = shakeArgs options $ do
             , "webpack.config.js"
             , ".babelrc"
             ]
-        cmd
-            "docker"
-            [ "run"
-            , "--rm"
-            , "-t"
-            , "-v"
-            , (pwd </> "ui") <> ":/work"
-            , "-w"
-            , "/work"
-            , "-e"
-            , "LOCAL_USER_ID=" <> uid
-            , "pankzsoft/haskell-base"
-            , "npm i && npm run build && npm test"
-            ]
+        cmd_ (Cwd "ui") "npm" ["i"]
+        cmd (Cwd "ui") "npm" ["run", "all"]
 
 needDirectoryFiles dir patterns =
     need =<< getDirectoryFiles "" ((dir </>) <$> patterns)
