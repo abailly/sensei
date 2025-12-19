f87a023de42d9b0bff55a6bfa9fe36b6eb3f69c7
Arnaud Bailly <arnaud@pankzsoft.com>
2025-12-19 22:49:38 +0100

    Add an install step to build

--- build.hs
+++ build.hs
@@ -8,17 +8,29 @@ build-depends: base
 -}
 
 import Control.Exception (throw)
-import Control.Monad (forM_, unless)
+import Control.Monad (forM_, unless, when)
 import Data.Functor
 import Data.Maybe
 import Development.Shake
-import System.Directory (getCurrentDirectory)
+import System.Directory (XdgDirectory (XdgData), createDirectoryIfMissing, getCurrentDirectory, getXdgDirectory, removePathForcibly)
 import System.Environment (getArgs, lookupEnv, withArgs)
 import System.FilePath
 import System.Posix.User (getRealUserID)
 
 -- | Id of current user
 type UID = String
+
+-- | Get XDG bin directory, defaulting to ~/.local/bin
+-- XDG_BIN_HOME is not part of the official XDG spec, but is a common convention
+getXdgBinDir :: IO FilePath
+getXdgBinDir = do
+  maybeXdgBinHome <- lookupEnv "XDG_BIN_HOME"
+  case maybeXdgBinHome of
+    Just binDir -> pure binDir
+    Nothing -> do
+      -- XdgData gives us ~/.local/share, so we go up one level and add bin
+      xdgDataDir <- getXdgDirectory XdgData ""
+      pure $ takeDirectory xdgDataDir </> "bin"
 
 options =
   shakeOptions
@@ -59,7 +71,28 @@ runShake pwd uid = shakeArgs options $ do
             "Makefile"
           ]
 
-  want ["bin/sensei-exe"]
+  -- Check if 'install' was requested
+  args <- liftIO getArgs
+  let wantInstall = "install" `elem` args
+
+  if wantInstall
+    then want ["install"]
+    else want ["bin/sensei-exe"]
+
+  "install" ~> do
+    need ["bin/sensei-exe"]
+    xdgBinDir <- liftIO getXdgBinDir
+    liftIO $ createDirectoryIfMissing True xdgBinDir
+    let targetExe = xdgBinDir </> "sensei-exe"
+        targetLink = xdgBinDir </> "ep"
+    putInfo $ "Installing sensei-exe to " <> targetExe
+    -- Use install with explicit permissions (755 = rwxr-xr-x)
+    cmd_ "install" ["-m", "755", "bin/sensei-exe", targetExe]
+    putInfo $ "Creating symlink " <> targetLink <> " -> sensei-exe"
+    -- Remove existing symlink if present
+    liftIO $ removePathForcibly targetLink
+    cmd_ [Cwd xdgBinDir] "ln" ["-sf", "sensei-exe", "ep"]
+    putInfo $ "Installation complete. Make sure " <> xdgBinDir <> " is in your PATH."
 
   "client/senseic" %> \clientbin -> do
     needClientSources
